716f765d8652230616081289a2a14f075e1c35f6
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-21 19:51:23 +0530

    count 1 bits of a given number

--- GeekForGeeks/Number_of_1_bits.cpp
+++ GeekForGeeks/Number_of_1_bits.cpp
@@ -2,19 +2,23 @@
 #include<iostream>
 using namespace std;
 
+int function1(int n){
+    int count = 0;
+    while(n){
+        if(n&1){
+            count++;
+        }
+        n = n >> 1;
+    }
+    return count;
+}
 int main(){
 
     int n;
     cin >> n;
 
-    int count = 0;
+    int ans = function1(n);
+    cout << ans;
 
-    while(n){
-        
-        count += n & 1;
-        
-        n >>= 1;
-    }
-    cout << count;
     return 0;
 }
