636133c91761afac09e089b11f590ea40a3a79cd
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-22 19:57:36 +0530

    😂😂😂😎

--- GeekForGeeks/Is_Binary_DIvisible_By_3.cpp
+++ GeekForGeeks/Is_Binary_DIvisible_By_3.cpp
@@ -4,16 +4,20 @@
 using namespace std;
 
 int IsBinaryDivisibleBy3(string s){
+
     int sum = 0; 
     int temp = 1;
 
     for(int i = s.length(); i >= 0; i--){
+
         if(s[i] - '0' == 1){
             sum = (sum + temp) % 3;
         }
         temp = (temp * 2) % 3;
     }
-    if(sum == 0) return 1;
+    if(sum == 0) 
+        return 1;
+
     return 0;
 }
 
