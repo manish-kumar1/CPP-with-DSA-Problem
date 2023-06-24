a4c61eddbdd3c6bf0ce7474038faf9417f56ba65
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-24 20:05:48 +0530

    hello meri jaan

--- Capgemini PseudoCode/005.cpp
+++ Capgemini PseudoCode/005.cpp
@@ -5,13 +5,16 @@ using namespace std;
 int main(){
 
     int p = 9, q = 9, r = 10;
+
     for(r = 3; r <= 4; r++){
         p = p + p;
     }
     for(r = 2; r <= 5; r++){
         p = r + p;
     }
+
     cout << p + q;
+    
     return 0;
 }
 
