d72d4090af1febf0ffef69b89298f732e6474168
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-25 20:07:24 +0530

    job

--- Capgemini PseudoCode/007.cpp
+++ Capgemini PseudoCode/007.cpp
@@ -6,11 +6,17 @@ using namespace std;
 int main(){
 
     int p;
+
     string str1;
+
     str1 = "Dehli";
+
     p = str1.size();
+
     int ans = (p^1);
+
     cout << ans;
+    
     return 0;
 }
 
