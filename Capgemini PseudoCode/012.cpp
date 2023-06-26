d6759dde37398bc3385fb275b965bd7e1821a35c
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-26 20:12:51 +0530

    modi ji

--- Capgemini PseudoCode/012.cpp
+++ Capgemini PseudoCode/012.cpp
@@ -6,15 +6,19 @@ using namespace std;
 int main(){
 
     int a, b, c;
+
     a = 7, b = 8, c = 7;
+
     c = (1 ^ 4) + c;
     a = ( b + 12 ) + c;
     b = (a + 2) & a;
 
     if((c & b) < b || 5 > b){
+
         b = c + a;
         a  = (c + c) + a;
     }
+    
     a = (b & 9) + a;
     a = (c + c) + a;
 
