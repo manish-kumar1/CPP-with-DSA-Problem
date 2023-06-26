0902bc9a6fd37b44858e7e0161ba4f54c536c16d
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-26 20:14:09 +0530

    hii

--- Capgemini PseudoCode/013.cpp
+++ Capgemini PseudoCode/013.cpp
@@ -8,12 +8,14 @@ int main(){
     a = 5, b = 5, c = 5;
 
     for(int c = 4; c <= 5; c++){
+        
         if(1 + 7 < 10 - a){
             a = a + b;
         }
         a = (a + c) + c;
         b = (11 & 12) + a;
     }
+
     cout<<a+b;
 
     return 0;
