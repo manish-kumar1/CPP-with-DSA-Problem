33a286e00a2ebe86401fa3008ead4c331991dd0b
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-06-26 20:09:56 +0530

    Good

--- Capgemini PseudoCode/008.cpp
+++ Capgemini PseudoCode/008.cpp
@@ -5,8 +5,11 @@ using namespace std;
 int main(){
 
     int m = 1;
+
     int a[4] = {1, 11, 21, 12};
+
     for(int j = 0; j <= 3; j++){
+
         if(j - 1 > a[j] + 3){
             break;
         }
@@ -14,7 +17,9 @@ int main(){
             m = a[j];
         }
     }
+
     cout<<m;
+    
     return 0;
 }
 
