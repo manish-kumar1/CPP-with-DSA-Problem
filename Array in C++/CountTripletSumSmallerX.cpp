0666fdd45c1788750f2b89169305d29d6256fdc6
manish-kumar1 <rmisterkumar13298@gmail.com>
2023-07-07 10:24:25 +0530

    m,mm

--- Array in C++/CountTripletSumSmallerX.cpp
+++ Array in C++/CountTripletSumSmallerX.cpp
@@ -6,8 +6,9 @@ long long CountTripletSumSmallerX(int arr[], int n, int X){
 
     sort(arr, arr+n);
     int count=0;
-    
+
     for(int i = 0; i < n; i++){
+        
         int left = i + 1;
         int right = n - 1;
         while(left < right){
