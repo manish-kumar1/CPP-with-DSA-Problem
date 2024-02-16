b51b37baf6ace235776832605b0d0839006bcba0
manish-kumar1 <rmisterkumar13298@gmail.com>
2024-02-16 23:43:02 +0530

    hj

--- Array in C++/RemoveDuplicateFromSortedArray.cpp
+++ Array in C++/RemoveDuplicateFromSortedArray.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int RemoveDuplicateFromSortedArray(vector<int> &ar){
-    
+
     int i = 0;
     for(int j = 1; j < ar.size(); j++){
         if(ar[i] != ar[j]){
@@ -21,6 +21,7 @@ int main(){
 
     int val = 0;
     vector<int> ar;
+    
     for(int i = 0; i < n; i++){
         cin >> val;
         ar.push_back(val);
